38e87b29fdfc73bee4691cd71aaf107d709bee6e
Lon Ingram <lawnsea@gmail.com>
2013-03-13 22:37:11 -0500

    Build template for snippet that users will include in their page
    The snippet injects an async script tag pointing to a widget-specific scout
    file. The scout file will then asynchronously inject the actual widget code
    using a versioned url.
    
    The scout should be served with a max-age equal to the longest amount of time a
    user should get stale widget code. The widget code should be served with headers
    that tell the browser to cache it forever. See Alex Sexton's explanation at:
    bit.ly/Yqmol1.

---
+++ Gruntfile.js
@@ -0,0 +1,96 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          waterfall: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+
+    render: {
+      'snippet': {
+        target: '<%= build.root %>/snippet.html',
+        source: './src/snippet.template',
+        scout: {
+          url: {
+            path: '//waterfallmobile.com/widget/{{ widgetId }}/',
+            file: 'waterfall-subscription-widget.scout.js'
+          },
+          listIdToken: '{{ listId }}'
+        }
+      },
+      'snippet-test': {
+        target: '<%= build.testRoot %>/snippet.html',
+        source: '<%= render.snippet.source %>',
+        scout: {
+          url: {
+            path: '/',
+            file: '<%= render.snippet.scout.url.file %>'
+          },
+          listIdToken: '<%= render.snippet.scout.listIdToken %>'
+        }
+      }
+    },
+
+    build: {
+      root: './release',
+      testRoot: './test/assets'
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-requirejs');
+
+  grunt.registerMultiTask('render', 'Render a static asset from a template.',
+    function renderTask() {
+      grunt.file.write(this.data.target,
+        grunt.template.process(grunt.file.read(this.data.source), {
+          data: this.data
+        }));
+    });
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+};
